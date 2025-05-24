3f674bb4e802e2ccff448d2c641e7b3c1176530f
Sng43 <daemmyoff1cial@gmail.com>
2025-05-24 13:13:21 +0200

    before numpy

--- math/linear_algebra/draft.py
+++ math/linear_algebra/draft.py
@@ -2,14 +2,19 @@
 
 def add(arr1, arr2):
 
-    if len(arr1) == len(arr2):
-        return [arr1[i] + arr2[i] for i in range(len(arr1))]   
+    if len(arr1) == len(arr2) and  len(arr1[0]) == len(arr2[0]):
+        return [[arr1[i][j] + arr2[i][j] for i in range(len(arr1[0]))]
+        for j in range(len(arr1))
+        ]
+
 
     else:
         return None
-arr1 = [1, 2, 3, 4]
-arr2 = [5, 6, 7, 8]
-print(add(arr1, arr2))
-# print(arr1)
-# print(arr2)
-# # print(add(arr1, [1, 2, 3]))
+
+
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[5, 6], [7, 8]]
+print(add(mat1, mat2))
+print(mat1)
+print(mat2)
+print(add(mat1, [[1, 2, 3], [4, 5, 6]]))
