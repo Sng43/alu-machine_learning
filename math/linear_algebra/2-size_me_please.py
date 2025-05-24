ddef8ed2fa68e0d420b27e7e21acd4f223509e0e
Sng43 <daemmyoff1cial@gmail.com>
2025-05-24 12:19:44 +0200

    size_her_up

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-'''
+"""
     A function def matrix_shape(matrix): that calculates the shape of a matrix
-'''
+"""
 
 
 def matrix_shape(matrix):
-    '''
-        Calculates the shape of a matrix
-    '''
+    """
+    Calculates the shape of a matrix
+    """
     mat_shape = []
     while isinstance(matrix, list):
         mat_shape.append(len(matrix))
