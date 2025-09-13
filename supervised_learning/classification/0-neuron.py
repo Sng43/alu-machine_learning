17098ca950bbe80526f12d35ab91e81d26982712
Sng-4 <s.kabare1@alustudent.com>
2025-09-13 10:38:43 +0200

    blacked

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-"""Class Neuron that defines a single neuron performing binary classification
-"""
+"""Class Neuron that defines a single neuron performing binary classification"""
 
 
 import numpy as np
 
 
 class Neuron:
-    """ Class Neuron
-    """
+    """Class Neuron"""
 
     def __init__(self, nx):
-        """ Instantiation function of the neuron
+        """Instantiation function of the neuron
 
         Args:
             nx (_type_): _description_
@@ -21,9 +19,9 @@ class Neuron:
             ValueError: _description_
         """
         if not isinstance(nx, int):
-            raise TypeError('nx must be an integer')
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
+            raise ValueError("nx must be a positive integer")
 
         self.W = np.random.normal(size=(1, nx))
         self.b = 0
